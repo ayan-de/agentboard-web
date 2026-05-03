02197512479cd4dbb5bf8a0f68dba4759333a140
Ayan De <deayan252@gmail.com>
2026-05-03 21:56:02 +0530

    feat: introduce Separator component and integrate it between homepage sections along with Waitlist component

---
+++ src/components/ui/Separator.tsx
@@ -0,0 +1,9 @@
+import Container from '@/components/ui/Container'
+
+export default function Separator() {
+  return (
+    <Container>
+      <div className="border-t border-[var(--foreground)]/10" />
+    </Container>
+  )
+}
